c9cbb8d39d1c307daf9f9197f9db14e17d528a07
EsAm1973 <me354172@gmail.com>
2025-09-13 15:25:52 +0300

    Implement onboarding navigation and add OnboardView with initial placeholder

--- lib/Core/utils/app_router.dart
+++ lib/Core/utils/app_router.dart
@@ -1,14 +1,19 @@
+import 'package:chatbox/Features/onboarding/presentation/views/onboard_view.dart';
 import 'package:chatbox/Features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const String kSplashRoute = '/';
-
+  static const String kOnboardRoute = '/onboard';
   static final router = GoRouter(
     routes: [
       GoRoute(
         path: kSplashRoute,
         builder: (context, state) => const SplashView(),
+      ),
+      GoRoute(
+        path: kOnboardRoute,
+        builder: (context, state) => const OnboardView(),
       ),
     ],
   );
--- lib/Features/splash/presentation/views/splash_view.dart
+++ lib/Features/splash/presentation/views/splash_view.dart
@@ -1,10 +1,29 @@
+import 'package:chatbox/Core/utils/app_router.dart';
 import 'package:chatbox/Core/utils/app_text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 import 'package:svg_flutter/svg.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    navigateToNextScreen();
+  }
+
+  void navigateToNextScreen() {
+    Future.delayed(const Duration(seconds: 2), () {
+      GoRouter.of(context).pushReplacement(AppRouter.kOnboardRoute);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
