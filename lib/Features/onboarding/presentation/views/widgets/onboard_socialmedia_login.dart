6cace69c4e1e031d4fcb10ab7f342bfcc92f5c23
EsAm1973 <me354172@gmail.com>
2025-09-13 16:16:05 +0300

    Add onboarding social media login buttons and enhance OnboardViewBody layout

---
+++ lib/Features/onboarding/presentation/views/widgets/onboard_socialmedia_login.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:svg_flutter/svg_flutter.dart';
+
+class OnboardSocialMediaLogin extends StatelessWidget {
+  const OnboardSocialMediaLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildSocialButton(
+          iconAssetPath: 'assets/facebook logo.svg',
+          onPressed: () {},
+          context: context,
+        ),
+        SizedBox(width: 20.w),
+
+        _buildSocialButton(
+          iconAssetPath: 'assets/google logo.svg',
+          onPressed: () {},
+          context: context,
+        ),
+        SizedBox(width: 20.w),
+
+        _buildSocialButton(
+          iconAssetPath: 'assets/apple logo.svg',
+          onPressed: () {},
+          context: context,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSocialButton({
+    required String iconAssetPath,
+    required VoidCallback onPressed,
+    required BuildContext context,
+  }) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        padding: EdgeInsets.all(10.r),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(
+            color: Theme.of(context).colorScheme.onSurface,
+            width: 1.w,
+          ),
+          color: Colors.black, // Background color of the circle
+        ),
+        child: SvgPicture.asset(
+          iconAssetPath,
+          width: 28.w,
+          height: 28.h,
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}
--- lib/Features/onboarding/presentation/views/widgets/onboard_view_body.dart
+++ lib/Features/onboarding/presentation/views/widgets/onboard_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:chatbox/Core/utils/app_text_styles.dart';
+import 'package:chatbox/Features/onboarding/presentation/views/widgets/onboard_socialmedia_login.dart';
 import 'package:chatbox/Features/onboarding/presentation/views/widgets/welcome_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -9,7 +11,24 @@ class OnboardViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 17.h),
-      child: Column(children: [WelcomeAppBar()]),
+      child: Column(
+        children: [
+          WelcomeAppBar(),
+          SizedBox(height: 43.h),
+          Text('Connect friends easily & quickly', style: AppTextStyles.bold68),
+          SizedBox(height: 16.h),
+          Text(
+            'Our chat app is the perfect way to stay connected with friends and family.',
+            style: AppTextStyles.regular16.copyWith(
+              color: Theme.of(
+                context,
+              ).colorScheme.onSurface.withValues(alpha: 0.7),
+            ),
+          ),
+          SizedBox(height: 38.h),
+          OnboardSocialMediaLogin(),
+        ],
+      ),
     );
   }
 }
