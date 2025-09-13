7afb522f5b1cb0199286b9f4b53fc7d621169212
EsAm1973 <me354172@gmail.com>
2025-09-13 17:39:58 +0300

    Add login and signup views with routing integration in AppRouter

--- lib/Core/utils/app_router.dart
+++ lib/Core/utils/app_router.dart
@@ -1,3 +1,5 @@
+import 'package:chatbox/Features/auth/presentation/views/login_view.dart';
+import 'package:chatbox/Features/auth/presentation/views/signup_view.dart';
 import 'package:chatbox/Features/onboarding/presentation/views/onboard_view.dart';
 import 'package:chatbox/Features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
@@ -5,6 +7,8 @@ import 'package:go_router/go_router.dart';
 abstract class AppRouter {
   static const String kSplashRoute = '/';
   static const String kOnboardRoute = '/onboard';
+  static const String kLoginRoute = '/login';
+  static const String kSignupRoute = '/signup';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -14,6 +18,14 @@ abstract class AppRouter {
       GoRoute(
         path: kOnboardRoute,
         builder: (context, state) => const OnboardView(),
+      ),
+      GoRoute(
+        path: kLoginRoute,
+        builder: (context, state) => const LoginView(),
+      ),
+      GoRoute(
+        path: kSignupRoute,
+        builder: (context, state) => const SignupView(),
       ),
     ],
   );
--- lib/Features/onboarding/presentation/views/widgets/onboard_view_body.dart
+++ lib/Features/onboarding/presentation/views/widgets/onboard_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:chatbox/Core/utils/app_router.dart';
 import 'package:chatbox/Core/utils/app_text_styles.dart';
 import 'package:chatbox/Core/widgets/custom_bottom.dart';
 import 'package:chatbox/Features/onboarding/presentation/views/widgets/login_row.dart';
@@ -6,6 +7,7 @@ import 'package:chatbox/Features/onboarding/presentation/views/widgets/or_divide
 import 'package:chatbox/Features/onboarding/presentation/views/widgets/welcome_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 
 class OnboardViewBody extends StatelessWidget {
   const OnboardViewBody({super.key});
@@ -41,10 +43,16 @@ class OnboardViewBody extends StatelessWidget {
               text: 'Sign up with email',
               backgroundColor: Theme.of(context).colorScheme.primary,
               textColor: Theme.of(context).colorScheme.onPrimary,
-              onTap: () {},
+              onTap: () {
+                GoRouter.of(context).pushReplacement(AppRouter.kSignupRoute);
+              },
             ),
             SizedBox(height: 30.h),
-            LoginRow(onTap: () {}),
+            LoginRow(
+              onTap: () {
+                GoRouter.of(context).pushReplacement(AppRouter.kLoginRoute);
+              },
+            ),
             SizedBox(height: 30.h),
           ],
         ),
