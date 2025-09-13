7b73dc4f08b4b8626b46c4d3a559af5c773fea02
EsAm1973 <me354172@gmail.com>
2025-09-13 14:02:11 +0300

    First Upload

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:chatbox/Core/utils/app_router.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: AppRouter.router,
+      debugShowCheckedModeBanner: false,
+      title: 'ChatBox',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+    );
+  }
+}
